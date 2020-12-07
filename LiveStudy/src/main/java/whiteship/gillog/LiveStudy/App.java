a4086a3a88f0f6ac4aa1e232cf9a131be35056a6
swgil007 <swgil007@naver.com>
2020-12-08 08:33:22 +0900

    live study first init

---
+++ LiveStudy/src/main/java/whiteship/gillog/LiveStudy/App.java
@@ -0,0 +1,13 @@
+package whiteship.gillog.LiveStudy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
